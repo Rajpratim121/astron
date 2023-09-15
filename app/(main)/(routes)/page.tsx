d89408f4fe256e8a6579ff8cb0db97277f05ce3e
Rajpratim <rajpratim.001@gmail.com>
2023-09-15 21:40:15 +0530

    Initial commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-indigo-500">
+        Hello Discord Clone
+      </h1>
+      <Button>Click Me</Button>
+    </>
+  );
+}
